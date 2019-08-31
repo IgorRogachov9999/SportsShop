ae6bb6770d2a2aa2afaed5c99c30400d486b1c83
unknown <igorrogachov9999@gmail.com>
2019-08-31 18:02:43 +0300

    Order administration added

--- SportsStore/Migrations/20190831143647_OrderAdministration.Designer.cs
+++ SportsStore/Migrations/20190831143647_OrderAdministration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportsStore.Models;
 
 namespace SportsStore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190831143647_OrderAdministration")]
+    partial class OrderAdministration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SportsStore/Migrations/20190831143647_OrderAdministration.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsStore.Migrations
+{
+    public partial class OrderAdministration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GiftWrap",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Line1",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Line2",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Line3",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Zip",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "State",
+                table: "Orders",
+                newName: "Address");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Orders",
+                newName: "State");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "GiftWrap",
+                table: "Orders",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Line1",
+                table: "Orders",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Line2",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Line3",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Zip",
+                table: "Orders",
+                nullable: true);
+        }
+    }
+}
